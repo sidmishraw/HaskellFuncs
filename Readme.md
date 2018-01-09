0d3b8720aa281f66c3f622493ecc63a028d7bda0
Sidharth Mishra <sidmishraw@gmail.com>
2018-01-09 00:53:03 -0800

    init commit 2

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# HaskellFuncs
+
+Author: Sidharth Mishra `<sidmishraw@gmail.com>`
+
+A catalogue of Haskell functions to keep myself sane.
--- array.hs
+++ array.hs
@@ -8,6 +8,7 @@
 -- @created Mon Jan 08 2018 22:59:10 GMT-0800 (PST)
 -- @last-modified Mon Jan 08 2018 22:59:10 GMT-0800 (PST)
 -}
+import Data.List (sort)
 
 -- | Reads data from the STDIN as a string of comma separated digits.
 -- Then, sorts the list of digits parsed from the input string.
@@ -21,21 +22,14 @@ readDigits s = sort $ map read (words (map replaceCommasAndBrackets s))
         replaceCommasAndBrackets ('}') = ' '
         replaceCommasAndBrackets c = c
 
--- | computeMissingRng : computes the missing ranges
-computeMissingRng :: [Int] -> Int -> Int -> String -> Bool -> String
-computeMissingRng (x1:x2:xs) s e rng isContinuous
-    | s > e || s > x1 || last xs > e = rng
-    | (x1 - s > 1) && (x2 - x1 == 1) = if (!isContinuous)
-        then computeMissingRng (x2:xs) (x1 + 1) e (rng ++ show s ++ "-" ++ show (x1 - 1) ++ ",") True
-        else computeMissingRng (x2:xs) s e rng True
-    | (x1 - s > 1) && (x2 - x1 > 1) = 
-    | (x1 - s == 1) && (x2 - x1 == 1)  = if (!isContinuous)
-        then computeMissingRng (x2:xs) (x1 + 1) e (rng ++ show s ++ ",") True
-        else computeMissingRng (x2:xs) (x1 + 1) e rng
-    | (x1 - s == 1) && (x2 - x1 > 1) = 
-    | x1 == s = computeMissingRng (x2:xs) (x1 + 1) e rng isContinuous -- do nothing scenario, ignore
-
+-- | Computes the missing ranges from the list, given the start and end points.
+computeMissingRng :: Int -> Int -> String -> [Int] -> String
+computeMissingRng start end rng (x:xs) = "range"
 
 
 main :: IO ()
-main = getLine >>= (putStrLn . computeMissingRng . readDigits)
+main = do
+    s <- getLine
+    start <- getLine
+    end <- getLine
+    (putStrLn . computeMissingRng (read start :: Int) (read end :: Int) "" . readDigits) s
